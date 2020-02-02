aaa6d87003040db19075150303007cc2f9313a8c
Nick Filmer <nick80835@gmail.com>
2020-02-02 08:45:49 -0500

    tweaked and fixed up incoming commands

--- ubot/command_handler.py
+++ ubot/command_handler.py
@@ -20,18 +20,32 @@ from telethon import events
 
 class CommandHandler():
     def __init__(self, client, logger):
-        self.commands = {}
+        self.outgoing_commands = {}
+        self.incoming_commands = {}
         self.logger = logger
         client.add_event_handler(self.handle_outgoing, events.NewMessage(outgoing=True))
+        client.add_event_handler(self.handle_incoming, events.NewMessage(incoming=True))
 
     async def handle_outgoing(self, event):
-        for cmd in self.commands.keys():
+        for cmd in self.outgoing_commands.keys():
             if search(cmd, event.text):
                 event.pattern_match = search(cmd, event.text)
 
                 try:
-                    await self.commands.get(cmd)(event)
+                    await self.outgoing_commands.get(cmd)(event)
                     return
                 except Exception as exception:
-                    self.logger.warn(f"{self.commands.get(cmd).__name__} - {exception}")
-                    await event.reply(f"`An error occurred in {self.commands.get(cmd).__name__}: {exception}`")
+                    self.logger.warn(f"{self.outgoing_commands.get(cmd).__name__} - {exception}")
+                    await event.reply(f"`An error occurred in {self.outgoing_commands.get(cmd).__name__}: {exception}`")
+
+    async def handle_incoming(self, event):
+        for cmd in self.incoming_commands.keys():
+            if search(cmd, event.text):
+                event.pattern_match = search(cmd, event.text)
+
+                try:
+                    await self.incoming_commands.get(cmd)(event)
+                    return
+                except Exception as exception:
+                    self.logger.warn(f"{self.incoming_commands.get(cmd).__name__} - {exception}")
+                    await event.reply(f"`An error occurred in {self.incoming_commands.get(cmd).__name__}: {exception}`")
--- ubot/loader.py
+++ ubot/loader.py
@@ -40,7 +40,8 @@ class Loader():
             self.loaded_modules.append(import_module("ubot.modules." + module_name))
 
     def reload_all_modules(self):
-        self.command_handler.commands = {}
+        self.command_handler.outgoing_commands = {}
+        self.command_handler.incoming_commands = {}
 
         errors = ""
 
@@ -55,7 +56,8 @@ class Loader():
 
     def add(self, **args):
         prefix = escape(self.settings.get_config("cmd_prefix") or '.')
-        args['incoming'] = True
+        outgoing = args.get('outgoing', True)
+        incoming = args.get('incoming', False)
 
         if args.get('noprefix', None):
             del args['noprefix']
@@ -68,7 +70,10 @@ class Loader():
             args['pattern'] = f"(?is)(.*){args['pattern']}(.*)"
 
         def decorator(func):
-            self.command_handler.commands[args['pattern']] = func
+            if incoming:
+                self.command_handler.incoming_commands[args['pattern']] = func
+            elif outgoing:
+                self.command_handler.outgoing_commands[args['pattern']] = func
 
         return decorator
 
