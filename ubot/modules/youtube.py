2f092a99cabdb280bb9d91a3105284b0d62d1569
Nick80835 <nick80835@gmail.com>
2020-04-02 12:15:15 -0400

    add simple youtube downloader

---
+++ ubot/modules/youtube.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import pafy
+
+from ubot.micro_bot import micro_bot
+
+ldr = micro_bot.loader
+
+
+@ldr.add(pattern="yt")
+async def youtube_cmd(event):
+    query = event.pattern_match.group(1)
+    video = pafy.new(query)
+    video_stream = video.getbest()
+    try:
+        await event.client.send_file(event.chat_id, file=video_stream.url)
+    except:
+        await event.reply(f"`Download failed: `[URL]({video_stream.url})")
+
+
+@ldr.add(pattern="yta")
+async def youtube_audio_cmd(event):
+    query = event.pattern_match.group(1)
+    video = pafy.new(query)
+    video_stream = video.getbestaudio()
+    try:
+        await event.client.send_file(event.chat_id, file=video_stream.url)
+    except:
+        await event.reply(f"`Download failed: `[URL]({video_stream.url})")
