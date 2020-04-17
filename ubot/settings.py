06030ad037c42719fd76c41e6956204583848433
Nick80835 <nick80835@gmail.com>
2020-04-17 11:18:23 -0400

    add optional default returns for settings

--- ubot/settings.py
+++ ubot/settings.py
@@ -8,11 +8,11 @@ class Settings():
         self.config = SafeConfigParser()
         self.config.read("settings.ini")
 
-    def get_config(self, key):
-        return self.config.get("DEFAULT", key, fallback=None)
+    def get_config(self, key, default=None):
+        return self.config.get("DEFAULT", key, fallback=default)
 
-    def get_bool(self, key):
-        return bool(self.config.get("DEFAULT", key, fallback=False) == "True")
+    def get_bool(self, key, default=False):
+        return bool(self.config.get("DEFAULT", key, fallback=default) == "True")
 
     def set_config(self, key, value):
         self.config.set("DEFAULT", key, value)
