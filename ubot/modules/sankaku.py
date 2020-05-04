7ca73b439974b2ad7f73bbc2b6a1165a72b7316e
Nick80835 <nick80835@gmail.com>
2020-05-03 20:45:35 -0400

    clean up scrapers a bit

--- ubot/modules/sankaku.py
+++ ubot/modules/sankaku.py
@@ -31,12 +31,11 @@ async def sankaku(event):
     async with session.get(SAN_URL, params=params) as response:
         if response.status == 200:
             response = await response.json()
+            await session.close()
         else:
             await event.reply(f"`An error occurred, response code: `**{response.status}**")
             await session.close()
             return
-
-    await session.close()
 
     if not response:
         await event.reply(f"`No results for query: `**{event.args}**")
@@ -44,9 +43,9 @@ async def sankaku(event):
 
     valid_urls = []
 
-    for item in response:
-        if 'file_url' in item.keys():
-            valid_urls.append(item['file_url'])
+    for post in response:
+        if 'file_url' in post.keys():
+            valid_urls.append(post['file_url'])
 
     if not valid_urls:
         await event.reply(f"`Failed to find URLs for query: `**{event.args}**")
