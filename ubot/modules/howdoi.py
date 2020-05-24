227b2c8b2c799128192ec9bab8af3b9d6ef0312a
Nick80835 <nick80835@gmail.com>
2020-05-24 16:21:47 -0400

    add no arg check to howdoi

--- ubot/modules/howdoi.py
+++ ubot/modules/howdoi.py
@@ -11,6 +11,10 @@ ldr = micro_bot.loader
 
 @ldr.add("hdi")
 async def howdoi_cmd(event):
+    if not event.args:
+        await event.edit(f"`Syntax: {ldr.settings.get_config('cmd_prefix') or '.'}hdi <question>`")
+        return
+
     response = howdoi.howdoi(vars(howdoi.get_parser().parse_args(event.args.split(' '))))
     response = re.sub(r'\n\n+', '\n\n', response).strip()
 
