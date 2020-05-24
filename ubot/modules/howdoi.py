9b296446b5eabcee5f7e89620a13f8f5898cca5d
Nick80835 <nick80835@gmail.com>
2020-05-24 13:41:39 -0400

    add howdoi

---
+++ ubot/modules/howdoi.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from howdoi import howdoi
+
+from ubot.micro_bot import micro_bot
+
+ldr = micro_bot.loader
+
+
+@ldr.add("hdi")
+async def howdoi_cmd(event):
+    response = howdoi.howdoi(vars(howdoi.get_parser().parse_args(event.args.split(' '))))
+    await event.edit(f"**Query:**\n`{event.args}`\n**Answer:**\n`{response}`")
