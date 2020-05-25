7153a5710d3624b7339dc8925909a4152222330f
Nick80835 <nick80835@gmail.com>
2020-05-25 14:04:56 -0400

    add support for inline article results

--- ubot/command_handler.py
+++ ubot/command_handler.py
@@ -13,10 +13,11 @@ class CommandHandler():
         self.inline_pattern_template = "(?is)^{0}(?: |$|_)(.*)"
         self.incoming_commands = {}
         self.inline_photo_commands = {}
+        self.inline_article_commands = {}
         self.logger = logger
         self.settings = settings
         client.add_event_handler(self.handle_incoming, events.NewMessage(incoming=True))
-        client.add_event_handler(self.handle_inline_photo, events.InlineQuery())
+        client.add_event_handler(self.handle_inline, events.InlineQuery())
 
     async def handle_incoming(self, event):
         prefix = escape(self.settings.get_config("cmd_prefix") or '.')
@@ -40,46 +41,81 @@ class CommandHandler():
                     await event.reply(f"`An error occurred in {value['function'].__name__}: {exception}`")
                     raise exception
 
-
-    async def handle_inline_photo(self, event):
-        pattern_match = None
-
+    async def handle_inline(self, event):
         for key, value in self.inline_photo_commands.items():
             pattern_match = search(self.inline_pattern_template.format(key), event.text)
 
             if pattern_match:
-                builder = event.builder
-                event.pattern_match = pattern_match
-                event.args = pattern_match.groups()[-1]
+                await self.handle_inline_photo(event, pattern_match, value)
+                return
 
-                url_list = await value["function"](event)
+        for key, value in self.inline_article_commands.items():
+            pattern_match = search(self.inline_pattern_template.format(key), event.text)
 
-                if not url_list:
-                    continue
+            if pattern_match:
+                await self.handle_inline_article(event, pattern_match, value)
+                return
 
-                photo_coros = []
+        await self.fallback_inline(event)
 
-                for url in url_list:
-                    try:
-                        photo_coros += [self.try_coro(builder.photo(url))]
-                    except:
-                        pass
+    async def handle_inline_photo(self, event, pattern_match, value):
+        builder = event.builder
+        event.pattern_match = pattern_match
+        event.args = pattern_match.groups()[-1]
 
-                if photo_coros:
-                    photos = await asyncio.gather(*photo_coros)
-                else:
-                    continue
+        url_list = await value["function"](event)
 
-                try:
-                    await event.answer([i for i in photos if i])
-                except:
-                    pass
+        if not url_list:
+            return
 
-        if not pattern_match:
+        photo_coros = []
+
+        for url in url_list:
             try:
-                await event.answer([await event.builder.article(title=key, text=f"{self.settings.get_config('cmd_prefix') or '.'}{value['default']}") for key, value in self.inline_photo_commands.items() if value["default"]])
+                photo_coros += [self.try_coro(builder.photo(url))]
             except:
                 pass
+
+        if photo_coros:
+            photos = await asyncio.gather(*photo_coros)
+        else:
+            return
+
+        try:
+            await event.answer([i for i in photos if i])
+        except:
+            pass
+
+    async def handle_inline_article(self, event, pattern_match, value):
+        builder = event.builder
+        event.pattern_match = pattern_match
+        event.args = pattern_match.groups()[-1]
+
+        result_list = await value["function"](event)
+
+        if not result_list:
+            return
+
+        articles = []
+
+        for result in result_list:
+            try:
+                articles += [await builder.article(title=result["title"], description=result["description"], text=result["text"])]
+            except:
+                pass
+
+        try:
+            await event.answer([i for i in articles if i])
+        except:
+            pass
+
+    async def fallback_inline(self, event):
+        defaults_dict = {**self.inline_photo_commands, **self.inline_article_commands}
+
+        try:
+            await event.answer([await event.builder.article(title=key, text=f"{self.settings.get_config('cmd_prefix') or '.'}{value['default']}") for key, value in defaults_dict.items() if value["default"]])
+        except:
+            pass
 
     async def try_coro(self, coro):
         try:
