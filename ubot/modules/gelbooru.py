2d1a8de608df7a0a91cfcabb64eb5092b5bdad66
Nick80835 <nick80835@gmail.com>
2020-06-06 11:45:03 -0400

    add sauce to dan, gel and san

--- ubot/modules/gelbooru.py
+++ ubot/modules/gelbooru.py
@@ -7,6 +7,7 @@ from ubot.micro_bot import micro_bot
 ldr = micro_bot.loader
 
 GEL_URL = "https://gelbooru.com/index.php"
+GEL_SAUCE_URL = "https://gelbooru.com/index.php?page=post&s=view&id="
 
 
 @ldr.add("gel(s|x|q|)(f|)")
@@ -43,15 +44,15 @@ async def gelbooru(event):
 
     for post in response:
         if 'file_url' in post.keys():
-            valid_urls.append(post['file_url'])
+            valid_urls.append([post['file_url'], post['id']])
 
     if not valid_urls:
         await event.reply(f"Failed to find URLs for query: {event.args}")
         return
 
-    for image_url in valid_urls:
+    for image in valid_urls:
         try:
-            await event.reply(file=image_url, force_document=as_file)
+            await event.reply(f"[sauce]({GEL_SAUCE_URL}{image[1]})", file=image[0], force_document=as_file)
             return
         except:
             pass
