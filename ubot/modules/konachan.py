53d90f1cff53184a86f582629f28f0d05a98d815
Nick80835 <nick80835@gmail.com>
2020-09-23 21:16:49 -0400

    add konachan

---
+++ ubot/modules/konachan.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from ubot import ldr
+
+KON_URL = "https://konachan.com/post.json"
+KON_SAUCE_URL = "https://konachan.com/post/show/"
+
+
+@ldr.add_list(["kon", "konx", "konq", "kons"], pattern_extra="(f|)", help="Fetches images from Konachan, takes tags as arguments.")
+async def konachan(event):
+    await event.edit(f"`Processing…`")
+    safety_arg = event.command[-1]
+    as_file = bool(event.other_args[0])
+    rating = ""
+
+    if safety_arg == "x":
+        rating = "rating:x"
+    elif safety_arg == "s":
+        rating = "rating:s"
+    elif safety_arg == "q":
+        rating = "rating:q"
+
+    params = {"limit": 3,
+              "tags": f"order:random {rating} {event.args}".strip().replace("  ", " ")}
+
+    async with ldr.aioclient.get(KON_URL, params=params) as response:
+        if response.status == 200:
+            response = await response.json()
+        else:
+            await event.edit(f"`An error occurred, response code: `**{response.status}**")
+            return
+
+    if not response:
+        await event.edit(f"`No results for query: `**{event.args}**")
+        return
+
+    valid_urls = []
+
+    for post in response:
+        if 'file_url' in post.keys():
+            valid_urls.append([post['file_url'], post['id']])
+
+    if not valid_urls:
+        await event.edit(f"`Failed to find URLs for query: `**{event.args}**")
+        return
+
+    for image in valid_urls:
+        try:
+            await event.client.send_message(event.chat_id, f"[sauce]({KON_SAUCE_URL}{image[1]})", file=image[0], force_document=as_file)
+            await event.delete()
+            return
+        except:
+            pass
+
+    await event.edit(f"`Failed to fetch media for query: `**{event.args}**")
